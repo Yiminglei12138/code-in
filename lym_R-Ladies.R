1b82c69f707f25a0e9cb09f7f81ab48a80cd7612
Yiminglei12138 <59857372+Yiminglei12138@users.noreply.github.com>
2020-01-20 15:57:39 +0800

    Add files via upload

---
+++ lym_R-Ladies.R
@@ -0,0 +1,32 @@
+library(reshape2)
+library(ggplot2)
+library(stringr)
+
+#Read data
+sj<-read.csv("https://raw.githubusercontent.com/benubah/r-community-explorer/master/docs/data/rladies.csv")
+dim(sj)
+
+#Activity created data for 2018 and 2019 respectively
+sj2018<-sj[str_which(sj$created,"2018"),]
+sj2019<-sj[str_which(sj$created,"2019"),]
+dim(sj2018)
+dim(sj2019)
+
+#Sum the total number of name, members, country, and city variables
+name<-c(sum(duplicated(sj2018$name)==FALSE),sum(duplicated(sj2019$name)==FALSE))
+name
+members<-c(sum(sj2018$members),sum(sj2019$members))
+members
+country<-c(sum(duplicated(sj2018$country)==FALSE),sum(duplicated(sj2019$country)==FALSE))
+country
+city<-c(sum(duplicated(sj2018$city)==FALSE),sum(duplicated(sj2019$city)==FALSE))
+city
+year<-c("2018","2019")
+year
+
+#Visualize bar charts of variables, comparing 2018 and 2019
+new_sj<-data.frame(year,name,members/100,country,city)
+new_sj
+new_sj<-melt(new_sj,id=c("year"))
+ggplot(data=new_sj,aes(x=variable,y=value,fill=year))+
+ geom_col(position = position_dodge())
